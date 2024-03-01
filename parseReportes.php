6e7c594f88e4c67b0be9c092c125c8582b4188bf
kochq <matukoch@live.com>
2024-03-01 14:31:16 -0300

    Agregando distancia geodesica

--- parseReportes.php
+++ parseReportes.php
@@ -12,6 +12,23 @@ function procesar_pos(string $pos): float {
     return $pos_procesada;
 }
 
+function distanciaGeodesica(float $lat1, float $long1, float $lat2, float $long2): float{ 
+
+	$degtorad = 0.01745329; 
+	$radtodeg = 57.29577951; 
+	
+	$dlong = ($long1 - $long2); 
+	$dvalue = (sin($lat1 * $degtorad) * sin($lat2 * $degtorad)) 
+	+ (cos($lat1 * $degtorad) * cos($lat2 * $degtorad) 
+	* cos($dlong * $degtorad)); 
+	
+	$dd = acos($dvalue) * $radtodeg; 
+	
+	$km = ($dd * 111.302); 
+	
+	return $km; 
+} 
+
 $csv = file("./out.csv");
 
 $filename = 'out2.csv';
@@ -41,12 +58,15 @@ for($i = 0; $i<count($csv); $i++) {
     $latM2 = procesar_pos($datos[2]);
     $lngM2 = procesar_pos($datos[3]);
 
+    $difM1 = distanciaGeodesica($latM1_1, $lngM1_1, $latM1, $lngM1);
+    $difM2 = distanciaGeodesica($latM2_1, $lngM2_1, $latM2, $lngM2);
+
     $difLatM1 = $latM1_1 - $latM1;
     $difLatM2 = $latM2_1 - $latM2;
     $difLngM1 = $lngM1_1 - $lngM1;
     $difLngM2 = $lngM2_1 - $lngM2;
 
-    fputcsv($handle, [$latM1, $lngM1, $latM2, $lngM2, $difLatM1, $difLngM1, $difLatM2, $difLngM2], ",");
+    fputcsv($handle, [$latM1, $lngM1, $latM2, $lngM2, $difM1, $difM2], ",");
 }
 
 fclose($handle);
